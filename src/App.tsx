83f4f66ceb41d2b2f2012fc93313bc2dd01396e0
Kilson <kilsonrs@gmail.com>
2021-03-18 14:58:24 -0300

    chore: add and using transactionContext

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root')
 export function App() {
@@ -18,13 +19,13 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -38,7 +38,8 @@ createServer({
 
     this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody);
-      return schema.create('transaction', data);
+      const createdAt = new Date();
+      return schema.create('transaction', {...data, createdAt});
     })
   }
 })
